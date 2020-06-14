b4cc32489c1dc8380f7bbd7ed659ce08b3a2f802
Daniil Riazanovskiy <riazanovskiy@gmail.com>
2020-06-14 16:06:14 +0100

    Add db connection caching

--- db.py
+++ db.py
@@ -1,7 +1,8 @@
-import sqlite3
 import random
-
+import sqlite3
+import threading
 from sqlite3 import Error
+from typing import Iterable
 
 create_table_players_q = """ CREATE TABLE IF NOT EXISTS players (
                                 id integer PRIMARY KEY,
@@ -11,57 +12,31 @@ create_table_words_q = """ CREATE TABLE IF NOT EXISTS words (
                                 author integer,
                                 room text,
                                 used integer ); """
-add_word_q = """ INSERT INTO words(word, author, room, used) VALUES("{}", {}, "{}", 0);"""
-get_word_q = """ SELECT word FROM words WHERE room="{}" AND used=0; """
+add_word_q = """ INSERT INTO words(word, author, room, used) VALUES(?, ?, ?, 0);"""
+get_word_q = """ SELECT word FROM words WHERE room=? AND used=0; """
 mark_word_used_q = """ UPDATE words
                        SET used=1
-                       WHERE (word="{}" AND room="{}");"""
+                       WHERE (word=? AND room=?);"""
 
-find_unused_word_q = """ SELECT word FROM words WHERE word="{}" AND used=0 AND room="{}"; """
-num_words_in_hat_q = """ SELECT COUNT(word) as num FROM words WHERE used=0 AND room="{}"; """
-add_player_q = """ INSERT INTO players(id, room) VALUES({}, "{}");"""
-find_player_room_q = """ SELECT room FROM players WHERE id={};"""
+find_unused_word_q = """ SELECT word FROM words WHERE word=? AND used=0 AND room=?; """
+num_words_in_hat_q = """ SELECT COUNT(word) as num FROM words WHERE used=0 AND room=?; """
+add_player_q = """ INSERT INTO players(id, room) VALUES(?, ?);"""
+find_player_room_q = """ SELECT room FROM players WHERE id=?;"""
 remove_player_room_q = """ DELETE FROM players
-                       WHERE (id={});"""
-room_count_q = """ SELECT COUNT(id) FROM players WHERE room="{}";"""
+                       WHERE (id=?);"""
+room_count_q = """ SELECT COUNT(id) FROM players WHERE room=?;"""
 
 
-def create_connection(db_file):
-    """ create a database connection to a SQLite database """
-    conn = None
+def try_execute(cursor: sqlite3.Cursor, sql: str, parameters: Iterable = ...):
     try:
-        conn = sqlite3.connect(db_file)
-    except Error as e:
-        print(e)
-    return conn
-
-
-def execute_sql(db_file, sql):
-    conn = create_connection(db_file)
-    try:
-        c = conn.cursor()
-        c.execute(sql)
-        conn.commit()
-        conn.close()
+        cursor.execute(sql, parameters)
         return True
     except Error as e:
         print(e)
         return False
 
 
-def execute_sql_select(db_file, sql):
-    conn = create_connection(db_file)
-    try:
-        c = conn.cursor()
-        c.execute(sql)
-        ans = c.fetchall()
-        c.close()
-        return ans
-    except Error as e:
-        print(e)
-
-
-def check_word(word):
+def check_word(word: str):
     def check_rus(word):
         for c in word:
             if not (('а' <= c <= 'я') or c == '-' or c == 'ё'):
@@ -80,10 +55,23 @@ def check_word(word):
     return check_rus(word) or check_en(word)
 
 
+def get_local_cursor(data, db_file):
+    def cursor():
+        if 'conn' not in data.__dict__:
+            # isolation_level = None enables autocommit mode
+            # see https://docs.python.org/3/library/sqlite3.html#sqlite3.Connection.isolation_level
+            data.conn = sqlite3.connect(db_file, isolation_level=None)
+            data.cursor = data.conn.cursor()
+        return data.cursor
+
+    return cursor
+
+
 class Hat:
     def __init__(self, db_file):
-        self.db_file = db_file
-        execute_sql(self.db_file, create_table_words_q)
+        self.data = threading.local()
+        self.cursor = get_local_cursor(self.data, db_file)
+        self.cursor().execute(create_table_words_q)
 
     @staticmethod
     def max_word_count():
@@ -98,36 +86,29 @@ class Hat:
         if not check_word(word)\
                 or len(word) > self.max_word_length() or self.words_in_hat(room) >= self.max_word_count():
             return False
-        else:
-            query = find_unused_word_q.format(word, room)
-            res = execute_sql_select(self.db_file, query)
-            if res:
-                return False
-            query = add_word_q.format(word, player_id, room)
-            return execute_sql(self.db_file, query)
+        if self.cursor().execute(find_unused_word_q, (word, room)).fetchall():
+            return False
+        return try_execute(self.cursor(), add_word_q, (word, player_id, room))
 
     def get_word(self, room):
-        words = execute_sql_select(self.db_file, get_word_q.format(room))
+        words = self.cursor().execute(get_word_q, (room,)).fetchall()
         if not words:
             return None
         word = random.choice(words)[0]
-        if execute_sql(self.db_file, mark_word_used_q.format(word, room)):
+        if try_execute(self.cursor(), mark_word_used_q, (word, room)):
             return word
         else:
-            return "Не удалось вернуть слово в шляпу"
+            return None
 
     def remove_word(self, word, room):
-        query = find_unused_word_q.format(word, room)
-        res = execute_sql_select(self.db_file, query)
-        if not res:
+        if not self.cursor().execute(find_unused_word_q, (word, room)).fetchall():
             return False
-        status = execute_sql(self.db_file, mark_word_used_q.format(word, room))
-        return status
+        return try_execute(self.cursor(), mark_word_used_q, (word, room))
 
     def words_in_hat(self, room):
-        words_num = execute_sql_select(self.db_file, num_words_in_hat_q.format(room))
+        words_num = self.cursor().execute(num_words_in_hat_q, (room,)).fetchall()
         if not words_num:
-            return ""
+            return None
         return words_num[0][0]
 
 
@@ -148,26 +129,27 @@ class HatWrapper:
 
 class Game:
     def __init__(self, db_file):
-        self.db_file = db_file
-        execute_sql(self.db_file, create_table_players_q)
+        self.data = threading.local()
+        self.cursor = get_local_cursor(self.data, db_file)
+        self.cursor().execute(create_table_players_q)
 
     def add_player(self, player_id, room):
-        execute_sql(self.db_file, add_player_q.format(player_id, room))
+        self.cursor().execute(add_player_q, (player_id, room))
 
     def leave_room(self, player_id):
-        execute_sql(self.db_file, remove_player_room_q.format(player_id))
+        self.cursor().execute(remove_player_room_q, (player_id,))
 
     def room_for_player(self, player_id):
-        rooms = execute_sql_select(self.db_file, find_player_room_q.format(player_id))
-        if rooms:
-            return rooms[0][0]
+        result = self.cursor().execute(find_player_room_q, (player_id,)).fetchall()
+        if result:
+            return result[0][0]
         else:
             return None
 
     def room_size(self, room):
-        rooms = execute_sql_select(self.db_file, room_count_q.format(room))
-        if rooms:
-            return rooms[0][0]
+        result = self.cursor().execute(room_count_q, (room,)).fetchall()
+        if result:
+            return result[0][0]
         else:
             return None
 
@@ -178,7 +160,7 @@ def start_game(db_file):
     return hat, game
 
 
-if __name__ == '__main__':
+def tests():
     hat, game = start_game("test165.db")
     assert game.room_for_player(1) is None
     game.add_player(1, "room1")
@@ -191,15 +173,19 @@ if __name__ == '__main__':
     assert hat.get_word("room1") is None
     assert hat.add_word("первое", 1, "room1")
     assert hat.add_word("второе", 1, "room1")
-    hat.words_in_hat("room1")
+    assert hat.words_in_hat("room1") == 2
     assert hat.add_word("треТье", 1, "room1")
     assert not hat.remove_word("кусь", "room1")
     assert hat.add_word("чеТвертое", 1, "room1")
     assert hat.add_word("пятое", 1, "room1")
     assert hat.remove_word("пятое", "room1")
-    assert not hat.add_word("djfkjsd", 1, "room1")
+    assert not hat.add_word("qcь", 1, "room1")
     assert hat.get_word("room1")
     assert hat.get_word("room1")
     assert hat.get_word("room1")
     assert hat.get_word("room1")
-    assert not hat.get_word("room1")
+    assert hat.get_word("room1") is None
+
+
+if __name__ == '__main__':
+    tests()
